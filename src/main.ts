720825cf8612b4b4bcddb634fcb7dfcef4a275e7
yatelu <yydys1224@163.com>
2023-05-11 17:00:41 +0800

    进行基本路由配置，导入静态资源

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+import '@/assets/css/reset.css'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
