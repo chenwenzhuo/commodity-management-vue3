892d7d537282eb1732fd0c4f08211f1c2564be39
yatelu <yydys1224@163.com>
2023-05-12 16:14:37 +0800

    完成Home组件样式、布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>Home</div>
+    <div>欢迎使用商品管理系统</div>
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+div {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 48px;
+  font-weight: bolder;
+}
 </style>
