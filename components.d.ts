618db4ef067316d875a48895c27a73f69d6244ee
yatelu <yydys1224@163.com>
2023-05-12 10:08:59 +0800

    完成主界面基本布局

--- components.d.ts
+++ components.d.ts
@@ -9,12 +9,15 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ElAside: typeof import('element-plus/es')['ElAside']
     ElButton: typeof import('element-plus/es')['ElButton']
-    ElCol: typeof import('element-plus/es')['ElCol']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElFooter: typeof import('element-plus/es')['ElFooter']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElHeader: typeof import('element-plus/es')['ElHeader']
     ElInput: typeof import('element-plus/es')['ElInput']
-    ElRow: typeof import('element-plus/es')['ElRow']
+    ElMain: typeof import('element-plus/es')['ElMain']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,11 +1,42 @@
 <template>
-    <h1>Admin</h1>
+    <div class="outer-wrapper">
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>Main</el-main>
+                <el-footer>Footer</el-footer>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.outer-wrapper {
+  height: 100%;
 
+  .el-container {
+    height: 100%;
+    background-color: #9a9494;
+
+    .el-aside {
+      background-color: #dedcdc;
+    }
+
+    .el-container {
+      .el-header {
+        height: 80px;
+        background-color: #add6ea;
+      }
+
+      .el-footer {
+        background-color: #add6ea;
+      }
+    }
+  }
+}
 </style>
