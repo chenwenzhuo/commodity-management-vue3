791d3b6d90fe74263ecd34f06c3d8dc9076c7e58
chenwenzhuo <Chenwz1996@163.com>
2023-05-14 00:42:41 +0800

    应用ElementPlus本地化语言包

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import {createApp} from 'vue'
 import {createPinia} from 'pinia'
 //持久化存储pinia中数据
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import ElementPlus from 'element-plus'
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 import '@/assets/css/reset.css'
 import 'element-plus/dist/index.css'
@@ -15,5 +18,7 @@ pinia.use(piniaPluginPersistedstate);
 
 app.use(pinia)
 app.use(router)
+
+app.use(ElementPlus, {locale: zhCn,});//ElementPlus本地化语言包
 
 app.mount('#app')
